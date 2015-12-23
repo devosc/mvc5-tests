681edb75b0286d7f081073defc3a9ceeb02eb921
devosc <devosc@gmail.com>
2015-12-23 15:01:42 -0600

    resolvable  traversable filter

--- src/Resolver/Resolver/ResolvableTest.php
+++ src/Resolver/Resolver/ResolvableTest.php
@@ -234,10 +234,41 @@ class ResolvableTest
         $mock = $this->getCleanAbstractMock(Resolver::class, ['resolvable', 'resolvableTest']);
 
         $mock->expects($this->once())
-            ->method('filter')
-            ->willReturn('foo');
+             ->method('filter')
+             ->willReturn('foo');
+
+        $mock->expects($this->any())
+             ->method('resolve');
 
         $this->assertEquals('foo', $mock->resolvableTest(new Filter('foo')));
+    }
+
+    /**
+     *
+     */
+    public function test_resolvable_filter_resolvable()
+    {
+        /** @var Resolver|Mock $mock */
+
+        $mock = $this->getMockForAbstractClass(Resolver::class);
+
+        $plugin = new Plugin('Mvc5\Config', [[function($foo) { return $foo; }]]);
+
+        $this->assertEquals('foo', $mock->resolvableTest(new Filter('foo', $plugin)));
+    }
+
+    /**
+     *
+     */
+    public function test_resolvable_filter_args_plugin()
+    {
+        /** @var Resolver|Mock $mock */
+
+        $mock = $this->getMockForAbstractClass(Resolver::class);
+
+        $plugin = new Args([function($foo) { return $foo; }]);
+
+        $this->assertEquals('foo', $mock->resolvableTest(new Filter('foo', $plugin)));
     }
 
     /**
