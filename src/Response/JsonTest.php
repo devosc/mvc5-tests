d97e9d59408b2a266129912d46d9166fdedb6f5d
devosc <devosc@gmail.com>
2016-05-28 21:59:24 -0500

    json response

---
+++ src/Response/JsonTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ */
+
+namespace Mvc5\Test\Response\Config;
+
+use Mvc5\Arg;
+use Mvc5\Http\Headers\Config as Headers;
+use Mvc5\Response\Json;
+use Mvc5\Test\Test\TestCase;
+
+class JsonTest
+    extends TestCase
+{
+    /**
+     *
+     */
+    function test_construct()
+    {
+        $data = ['PHP' => phpversion(), 'System' => php_uname()];
+
+        $response = new Json($data);
+
+        $this->assertEquals((object) $data, json_decode($response[Arg::BODY]));
+        $this->assertEquals(Arg::HTTP_OK, $response->status());
+        $this->assertEquals(new Headers(['Content-Type' => 'application/json']), $response->headers());
+    }
+}
