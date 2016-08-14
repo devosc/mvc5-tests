14528a967b556c1ef46129b530c8ca6ec58c690d
devosc <devosc@gmail.com>
2016-08-13 23:24:07 -0500

    Fix PHPDoc

--- src/Signal.php
+++ src/Signal.php
@@ -37,8 +37,8 @@ class Signal
     }
 
     /**
-     * @param ...$args
-     * @return mixed
+     * @param array ...$args
+     * @return array|null
      */
     static function variadicArgsTest(...$args)
     {
