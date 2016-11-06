0238fadf8300dc6eedc8a96917ca4fcdce9906d5
devosc <devosc@gmail.com>
2016-11-06 00:27:01 -0500

    middleware uses named arguments

--- src/MiddlewareTest.php
+++ src/MiddlewareTest.php
@@ -6,6 +6,7 @@
 namespace Mvc5\Test;
 
 use Mvc5\App;
+use Mvc5\Middleware;
 use Mvc5\Request\Config as Request;
 use Mvc5\Response\Config as Response;
 use Mvc5\Test\Test\TestCase;
@@ -16,42 +17,40 @@ class MiddlewareTest
     /**
      *
      */
-    function test_construct()
+    function test_empty_stack_with_next()
     {
-        $this->assertInstanceOf(Middleware::class, new Middleware([]));
+        $middleware = new Middleware;
+        $request    = new Request;
+        $response   = new Response;
+
+        $next = function($request, $response) {
+            return 'foo';
+        };
+
+        $this->assertEquals('foo', $middleware($request, $response, $next));
     }
 
     /**
      *
      */
-    function test_next()
+    function test_empty_stack_without_next_returns_response()
     {
-        $middleware = new Middleware();
+        $middleware = new Middleware;
+        $request    = new Request;
+        $response   = new Response;
 
-        $this->assertInstanceOf(\Closure::class, $middleware->next());
+        $this->assertEquals($response, $middleware($request, $response));
     }
 
     /**
      *
      */
-    function test_invoke_string_response()
+    function test_pipe()
     {
-        $middleware = new Middleware([
-            function(Request $request, Response $response, callable $next) {
-                return 'foo';
-            }
-        ]);
+        $next = function($request, $response) {
+            return 'foo';
+        };
 
-        $middleware->service(new App);
-
-        $this->assertEquals('foo', $middleware(new Request, new Response));
-    }
-
-    /**
-     *
-     */
-    function test_invoke_response()
-    {
         $middleware = new Middleware([
             function(Request $request, Response $response, callable $next) {
                 return $next($request, $response);
@@ -60,19 +59,21 @@ class MiddlewareTest
 
         $middleware->service(new App);
 
-        $this->assertInstanceOf(Response::class, $middleware(new Request, new Response));
+        $this->assertEquals('foo', $middleware(new Request, new Response, $next));
     }
 
     /**
      *
      */
-    function test_invoke_empty_stack()
+    function test_no_pipe_returns_response()
     {
-        $middleware = new Middleware;
+        $middleware = new Middleware([
+            function(Request $request, Response $response, callable $next) {
+                return $next($request, $response);
+            }
+        ]);
 
         $middleware->service(new App);
-
-        $this->setExpectedException(\Exception::class, 'Empty call stack');
 
         $this->assertInstanceOf(Response::class, $middleware(new Request, new Response));
     }
