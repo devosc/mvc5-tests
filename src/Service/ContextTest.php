22fcf486b9edc7b02633b93392bb00c7da55f79e
devosc <devosc@gmail.com>
2016-11-28 00:51:28 -0600

    static service context

---
+++ src/Service/ContextTest.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ *
+ */
+
+namespace Mvc5\Test\Service;
+
+use Mvc5\App;
+use Mvc5\Service\Context;
+use Mvc5\Test\Test\TestCase;
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class ContextTest
+    extends TestCase
+{
+    /**
+     *
+     */
+    function test_constructor()
+    {
+        $app = new App;
+
+        $context = new Context($app);
+
+        $this->assertEquals($app, $context->service());
+    }
+
+    /**
+     *
+     */
+    function test_bind()
+    {
+        $app = new App;
+
+        Context::bind($app);
+
+        $this->assertEquals($app, ServiceFacade::service());
+    }
+
+    /**
+     *
+     */
+    function test_bind_exception()
+    {
+        $app = new App;
+
+        Context::bind($app);
+
+        $this->setExpectedException(\RuntimeException::class, 'Service already exists');
+
+        Context::bind($app);
+    }
+
+    /**
+     *
+     */
+    function test_service_does_not_exist()
+    {
+        $this->setExpectedException(\RuntimeException::class, 'Service does not exist');
+
+        Context::service();
+    }
+
+    /**
+     *
+     */
+    function test_invoke()
+    {
+        $app = new App;
+
+        $context = new Context;
+
+        call_user_func($context, $app);
+
+        $this->assertEquals($app, $context->service());
+    }
+}
