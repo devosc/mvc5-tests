4f429c4fe0188056b8b03776503df5d5183edc2f
devosc <devosc@gmail.com>
2017-02-13 06:59:58 -0600

    domain exception

---
+++ src/Exception/DomainTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *
+ */
+
+namespace Mvc5\Test\Exception;
+
+use Mvc5\Exception;
+use Mvc5\Exception\Domain;
+use Mvc5\Test\Test\TestCase;
+
+class DomainTest
+    extends TestCase
+{
+    /**
+     *
+     */
+    function test_domain_exception()
+    {
+        try {
+
+            Exception::domain('foo');
+
+        } catch(\Exception $exception) {}
+
+        $this->assertEquals('foo', $exception->getMessage());
+        $this->assertEquals(__FILE__, $exception->getFile());
+        $this->assertEquals(22, $exception->getLine());
+        $this->assertInstanceOf(Domain::class, $exception);
+    }
+
+    /**
+     *
+     */
+    function test_php_domain_exception()
+    {
+        try {
+
+            PHPException::domain('foo');
+
+        } catch(\Exception $exception) {}
+
+        $this->assertEquals('foo', $exception->getMessage());
+        $this->assertEquals(__FILE__, $exception->getFile());
+        $this->assertEquals(39, $exception->getLine());
+        $this->assertInstanceOf(\DomainException::class, $exception);
+    }
+}
