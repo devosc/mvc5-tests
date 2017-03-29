87239cbcb2d76d1e3655999420fea286d521b1a1
devosc <devosc@gmail.com>
2017-03-29 07:30:44 -0500

    host first

--- src/Http/HeadersTest.php
+++ src/Http/HeadersTest.php
@@ -16,8 +16,11 @@ class HeadersTest
      */
     function test()
     {
-        $headers = new Headers(['Host' => 'foo']);
+        $headers = new Headers(['bar' => 'baz', 'Host' => 'foo']);
 
+        $this->assertEquals('foo', $headers->current());
+        $this->assertTrue(isset($headers['bar']));
+        $this->assertEquals('baz', $headers['bar']);
         $this->assertTrue(isset($headers['Host']));
         $this->assertEquals('foo', $headers['host']);
 
@@ -25,9 +28,12 @@ class HeadersTest
 
         $this->assertFalse(isset($headers['Host']));
 
-        $with = $headers->with('Host', 'foo');
+        $with = $headers->with(['bar' => 'baz', 'Host' => 'foo']);
 
         $this->assertFalse(isset($headers['Host']));
+        $this->assertEquals('foo', $with->current());
+        $this->assertTrue(isset($with['bar']));
+        $this->assertEquals('baz', $with['bar']);
         $this->assertTrue(isset($with['Host']));
         $this->assertEquals('foo', $with['host']);
 
