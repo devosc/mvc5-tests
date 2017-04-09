4122ddad48745174c2ce03a557277f5528787767
devosc <devosc@gmail.com>
2017-04-08 23:40:37 -0500

    merge route name and params, urls without routes.

--- src/Url/PluginTest.php
+++ src/Url/PluginTest.php
@@ -6,6 +6,7 @@
 namespace Mvc5\Test\Url;
 
 use Mvc5\Arg;
+use Mvc5\Http\Uri\Config as HttpUri;
 use Mvc5\Request\Config as Request;
 use Mvc5\Test\Test\TestCase;
 use Mvc5\Url\Generator;
@@ -44,6 +45,34 @@ class PluginTest
     {
         $url = new Plugin(new Request, new Generator($this->route));
 
-        $this->assertEquals('/foo', $url('app', ['controller' => 'foo']));
+        $this->assertEquals('/foo', $url(['app', 'controller' => 'foo']));
+    }
+
+    /**
+     *
+     */
+    function test_no_route_config()
+    {
+        $route = [
+            Arg::NAME => 'app',
+            Arg::PATH => '/foo'
+        ];
+
+        $url = new Plugin(new Request([Arg::NAME => 'app']), new Generator($route));
+
+        //$this->assertEquals('/foo', $url());
+        $this->assertEquals('/app.html?foo=bar#top', $url('/app.html', ['foo' => 'bar'], 'top'));
+    }
+
+    /**
+     *
+     */
+    function test_uri()
+    {
+        $url = new Plugin(new Request, new Generator($this->route));
+
+        $uri = new HttpUri(['path' => '/app.html', 'query' => ['foo' => 'bar'], 'fragment' => 'top']);
+
+        $this->assertEquals('/app.html?foo=bar#top', $url($uri));
     }
 }
