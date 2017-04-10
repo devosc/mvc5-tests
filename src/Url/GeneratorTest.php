14dfc70adfcee4e85a95402cac8136b245b2f577
devosc <devosc@gmail.com>
2017-04-09 22:48:20 -0500

    tidy up

--- src/Url/GeneratorTest.php
+++ src/Url/GeneratorTest.php
@@ -5,8 +5,6 @@
 
 namespace Mvc5\Test\Url;
 
-use Mvc5\Arg;
-use Mvc5\Route\Config as Route;
 use Mvc5\Test\Test\TestCase;
 use Mvc5\Url\Generator;
 
@@ -19,20 +17,22 @@ class GeneratorTest
     function test_route()
     {
         $route = [
-            'app' => new Route([
-                Arg::NAME   => 'app',
-                Arg::PATH   => '/foo',
-                Arg::SCHEME => 'http',
-                Arg::HOST   => 'localhost',
-                Arg::PORT   => '8000',
-                Arg::WILDCARD => true,
-                ARG::TOKENS   => [['literal','/foo']]
-            ])
+            'app' => [
+                'scheme' => 'http',
+                'host'   => 'localhost',
+                'port'   => '8000',
+                'path'   => '/foo',
+                'children' => [
+                    'bar' => [
+                        'path' => '/bar'
+                    ]
+                ]
+            ]
         ];
 
         $generator = new Generator($route);
 
-        $this->assertEquals('http://localhost:8000/foo/bar/baz', (string) $generator('app', ['bar' => 'baz']));
+        $this->assertEquals('http://localhost:8000/foo/bar', (string) $generator('app/bar'));
     }
 
     /**
