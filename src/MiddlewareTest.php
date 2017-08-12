ef05d5b284cb17cddb84e11d36f240441e7dc30d
devosc <devosc@gmail.com>
2017-08-12 11:09:16 -0500

    middleware with multiple params

--- src/MiddlewareTest.php
+++ src/MiddlewareTest.php
@@ -6,9 +6,8 @@
 namespace Mvc5\Test;
 
 use Mvc5\App;
+use Mvc5\Config;
 use Mvc5\Middleware;
-use Mvc5\Http\HttpRequest;
-use Mvc5\Http\HttpResponse;
 use Mvc5\Test\Test\TestCase;
 
 class MiddlewareTest
@@ -17,33 +16,58 @@ class MiddlewareTest
     /**
      *
      */
-    function test_empty_stack_returns_response()
+    function test_empty_stack_with_no_args()
     {
-        $middleware = new Middleware(new App);
-        $request    = new HttpRequest;
-        $response   = new HttpResponse;
-
-        $this->assertEquals($response, $middleware($request, $response));
+        $this->assertNull((new App)->call(new Middleware(new App)));
     }
 
     /**
      *
      */
-    function test_reset()
+    function test_empty_stack_returns_last_param()
+    {
+        $this->assertEquals('baz', (new Middleware(new App))('foo', 'bar', 'baz'));
+    }
+
+    /**
+     *
+     */
+    function test_array_stack_with_reset()
     {
         $middleware = new Middleware(
             new App,
             [
-                function(HttpRequest $request, HttpResponse $response, callable $next) {
-                    return $next($request, $response);
+                function ($value, $next) {
+                    return $next($value . 'a');
                 },
-                function(HttpRequest $request, HttpResponse $response, callable $next) {
-                    return $next($request, $response);
+                function ($value, $next) {
+                    return $next($value . 'z');
                 }
             ]
         );
 
-        $this->assertInstanceOf(HttpResponse::class, $middleware(new HttpRequest, new HttpResponse));
-        $this->assertInstanceOf(HttpResponse::class, $middleware(new HttpRequest, new HttpResponse));
+        $this->assertEquals('baz', $middleware('b'));
+        $this->assertEquals('baz', $middleware('b'));
+    }
+
+    /**
+     *
+     */
+    function test_iterator_stack_with_reset()
+    {
+        $middleware = new Middleware(
+            new App,
+            new Config([
+                function ($value, $next) {
+                    return $next($value . 'a');
+                },
+                function ($value, $next) {
+                    return $next($value . 'z');
+                }
+            ])
+        );
+
+        $this->assertEquals('baz', $middleware('b'));
+        $this->assertEquals('baz', $middleware('b'));
     }
 }
