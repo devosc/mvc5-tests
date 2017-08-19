5ded6129c3a4a3d381d76e3d86c83825fa07ef05
devosc <devosc@gmail.com>
2017-08-19 11:45:31 -0500

    tidy up

--- src/Event/MiddlewareEvent.php
+++ src/Event/MiddlewareEvent.php
@@ -5,70 +5,23 @@
 
 namespace Mvc5\Test\Event;
 
-use Mvc5\Iterator;
+use Mvc5\Config\Iterator;
 use Mvc5\Event\Event;
 use Mvc5\Event\EventModel;
-use Mvc5\Service\Service;
+use Mvc5\Service\Middleware;
 
 class MiddlewareEvent
-    extends Iterator
-    implements Event
+    implements \Countable, Event, \Iterator
 {
     /**
      *
      */
     use EventModel;
-
-    /**
-     * @var Service
-     */
-    protected $service;
-
-    /**
-     * @param Service $service
-     * @param array|\Iterator $config
-     */
-    function __construct(Service $service, $config = [])
-    {
-        parent::__construct($config);
-        $this->service = $service;
-    }
-
-    /**
-     * @param array $args
-     * @return array
-     */
-    protected function args(array $args)
-    {
-        $args[] = function(...$args) {
-            return ($middleware = $this->next()) ? $this->call($middleware, $args) : ($args ? end($args) : null);
-        };
-
-        return $args;
-    }
-
-    /**
-     * @param $middleware
-     * @param array $args
-     * @param callable $callback
-     * @return mixed
-     */
-    protected function call($middleware, $args, callable $callback = null)
-    {
-        return $this->service->call($middleware, $this->args($args), $callback);
-    }
-
-    /**
-     * @return mixed
-     */
-    function next()
-    {
-        if ($this->config instanceof \Iterator) {
-            $this->config->next();
-            return $this->config->current();
-        }
-
-        return next($this->config);
+    use Iterator;
+    use Middleware {
+        Middleware::next insteadof Iterator;
+        next as public;
+        rewind as reset;
     }
 
     /**
@@ -77,7 +30,7 @@ class MiddlewareEvent
     function rewind()
     {
         $this->stopped = false;
-        $this->config instanceof \Iterator ? $this->config->rewind() : reset($this->config);
+        $this->reset();
     }
 
     /**
@@ -88,7 +41,7 @@ class MiddlewareEvent
      */
     function __invoke($callable, array $args = [], callable $callback = null)
     {
-        $result = $this->call($callable, $args, $callback);
+        $result = $this->call($callable, $args);
 
         $this->stop();
 
