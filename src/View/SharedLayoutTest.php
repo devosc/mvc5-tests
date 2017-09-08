6a5ddabe94e988b7a857f189c599a6708ac9281f
devosc <devosc@gmail.com>
2017-09-08 07:11:20 -0500

    SharedLayout

---
+++ src/View/SharedLayoutTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ */
+
+namespace Mvc5\Test\View;
+
+use Mvc5\App;
+use Mvc5\Test\Test\TestCase;
+use Mvc5\View\SharedLayout;
+
+class SharedLayoutTest
+    extends TestCase
+{
+    /**
+     *
+     */
+    function test_shared_layout()
+    {
+        $layout = new SharedLayout;
+
+        $this->assertSame($layout, $layout->with('foo', 'bar'));
+        $this->assertSame($layout, $layout->without('foo'));
+        $this->assertSame($layout, $layout->withService(new App));
+    }
+}
