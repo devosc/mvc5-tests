c9249135b0437331e8449da6dab5906f81a18eae
devosc <devosc@gmail.com>
2018-04-15 09:58:21 -0500

    null value plugin

--- src/Plugin/NullValueTest.php
+++ src/Plugin/NullValueTest.php
@@ -5,6 +5,7 @@
 
 namespace Mvc5\Test\Plugin;
 
+use Mvc5\App;
 use Mvc5\Plugin\NullValue;
 use Mvc5\Test\Test\TestCase;
 
@@ -16,6 +17,12 @@ class NullValueTest
      */
     function test()
     {
-        $this->assertNull((new NullValue)->config());
+        $app = new App(['services' => ['foo' => null, 'bar' => new NullValue]]);
+
+        $this->assertNull($app->plugin('bar'));
+
+        $this->expectExceptionMessage('Class foo does not exist');
+
+        $app->plugin('foo');
     }
 }
