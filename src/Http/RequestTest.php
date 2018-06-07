b1b5c4fb464a3d2b2b4cdfdef56eee36f9496374
devosc <devosc@gmail.com>
2018-06-06 22:40:28 -0500

    optional uri

--- src/Http/RequestTest.php
+++ src/Http/RequestTest.php
@@ -6,6 +6,8 @@
 namespace Mvc5\Test\Http;
 
 use Mvc5\Arg;
+use Mvc5\Http\HttpUri;
+use Mvc5\Http\Uri;
 use Mvc5\Http\HttpRequest;
 use Mvc5\Test\Test\TestCase;
 
@@ -47,9 +49,45 @@ class RequestTest
      */
     function test_uri()
     {
+        $request = new HttpRequest([Arg::URI => new HttpUri([Arg::PATH => 'foo'])]);
+
+        $this->assertInstanceOf(Uri::class, $request->uri());
+
+        $this->assertEquals('foo', $request->uri()['path']);
+    }
+
+    /**
+     *
+     */
+    function test_uri_array()
+    {
         $request = new HttpRequest([Arg::URI => [Arg::PATH => 'foo']]);
 
-        $this->assertEquals([Arg::PATH => 'foo'], $request->uri());
+        $this->assertInstanceOf(Uri::class, $request->uri());
+
+        $this->assertEquals('foo', $request->uri()['path']);
+    }
+
+    /**
+     *
+     */
+    function test_uri_string()
+    {
+        $request = new HttpRequest([Arg::URI => 'foo']);
+
+        $this->assertInstanceOf(Uri::class, $request->uri());
+
+        $this->assertEquals('foo', $request->uri()['path']);
+    }
+
+    /**
+     *
+     */
+    function test_uri_optional()
+    {
+        $request = new HttpRequest;
+
+        $this->assertNull($request->uri());
     }
 
     /**
