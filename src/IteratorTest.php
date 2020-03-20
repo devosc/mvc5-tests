6b0cf7ffa6920f3c700313f446c9774b39e14c43
devosc <devosc@gmail.com>
2020-03-20 16:31:20 -0500

    clone iterator

---
+++ src/IteratorTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *
+ */
+
+namespace Mvc5\Test;
+
+use Mvc5\Config;
+use Mvc5\Iterator;
+
+use Mvc5\Test\Test\TestCase;
+
+class IteratorTest
+    extends TestCase
+{
+    /**
+     *
+     */
+    function test_clone()
+    {
+        $config = new Config(['foo' => 'bar']);
+        $iterator = new Iterator($config);
+
+        $clone = clone $iterator;
+        $config['foo'] = 'foobar';
+
+        $this->assertEquals('foobar', $iterator->current());
+        $this->assertEquals('bar', $clone->current());
+    }
+}
